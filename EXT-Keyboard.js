8f9849e5c593dcabb5375a81577bc1a28a88d99d
bugsounet <bugsounet@bugsounet.fr>
2022-11-26 22:34:45 +0100

    first

---
+++ EXT-Keyboard.js
@@ -0,0 +1,73 @@
+/*********************
+*  EXT-Keyboard v1.0 *
+*  Bugsounet         *
+*  11/2022           *
+**********************/
+
+Module.register("EXT-Keyboard", {
+  defaults: {
+    debug: false,
+    keyFinder: false,
+    keys: []
+  },
+
+  start: function() {
+    this.resources = "/modules/EXT-Keyboard/resources/"
+    this.audio = null
+  },
+
+  getDom: function() {
+    var wrapper = document.createElement("div")
+    wrapper.style.display = 'none'
+    return wrapper
+  },
+
+  notificationReceived: function(noti, payload, sender) {
+    switch(noti) {
+      case "DOM_OBJECTS_CREATED":
+        this.sendSocketNotification("INIT", this.config)
+        this.prepare()
+        break
+      case "GAv4_READY": // send HELLO to Gateway ... (mark plugin as present in GW db)
+        if (sender.name == "MMM-GoogleAssistant") this.sendNotification("EXT_HELLO", this.name)
+        break
+    }
+  },
+
+  socketNotificationReceived: function(noti, payload) {
+    switch(noti) {
+      case "WARNING":
+        this.sendNotification("EXT_ALERT", {
+          type: "warning",
+          message: payload.message,
+          sound: this.resources + "keyboard.mp3"
+        })
+        break 
+    }
+  },
+
+  prepare: function() {
+    this.audio = new Audio()
+    this.audio.autoplay = true
+    onkeydown = (event) => {
+      if (this.config.keyFinder) {
+        this.sendNotification("EXT_ALERT", {
+          type: "information",
+          message: "You pressed: " + (event.key == " " ? "Space" : event.key) + ". keyCode is: " + event.keyCode,
+          timer: 3000,
+          sound: this.resources + "keyboard.mp3"
+        })
+      }
+      if (this.config.keys.length && Array.isArray(this.config.keys)) {
+        this.config.keys.forEach( key => {
+          if (key.keyCode == event.keyCode) {
+            if (key.notification) this.sendNotification(key.notification, key.payload || undefined)
+            if (key.command) this.sendSocketNotification("SHELLEXEC", key.command)
+            if (key.sound) this.audio.src = this.resources + key.sound + ".mp3"
+          }
+        })
+      }
+    }
+  }
+
+});
---
+++ node_helper.js
@@ -0,0 +1,60 @@
+/**********************
+*  EXT-Keyboard v1.0  *
+*  Bugsounet          *
+*  11/2022            *
+***********************/
+
+const exec = require("child_process").exec
+const path = require("path")
+
+var log = () => { /* do nothing */ };
+
+var NodeHelper = require("node_helper");
+
+module.exports = NodeHelper.create({
+  initialize: function(payload) {
+    console.log("[KEYBOARD] EXT-Keyboard Version:", require('./package.json').version, "rev:", require('./package.json').rev)
+    this.config = payload
+    if (this.config.debug) {
+      log = (...args) => { console.log("[KEYBOARD]", ...args) }
+    }
+    if (this.config.keys.length && Array.isArray(this.config.keys)) {
+      log("keys:", this.config.keys)
+    } else { 
+      console.log("[KEYBOARD] No keys found in config!")
+      this.sendSocketNotification("WARNING", { message: "No keys found in config!"} )
+    }
+  },
+
+  socketNotificationReceived: function(noti, payload) {
+    switch (noti) {
+      case "INIT":
+        this.initialize(payload)
+        break
+      case "SHELLEXEC":
+        let cwdPath = path.resolve(__dirname, "scripts/")
+        var command = payload
+        if (!command) {
+          this.sendSocketNotification("WARNING", { message: "ShellExec: no command to execute!"} )
+          return console.log("[KEYBOARD] ShellExec: no command to execute!")
+        }
+        exec (command, { cwd: cwdPath }, (e,so,se)=> {
+          log("ShellExec command:", command)
+          if (e) {
+            console.log("[KEYBOARD] ShellExec Error:" + e)
+            this.sendSocketNotification("WARNING", { message: "ShellExec: execute Error !"} )
+          }
+
+          log("SHELLEXEC_RESULT", {
+            executed: payload,
+            result: {
+              error: e,
+              stdOut: so,
+              stdErr: se,
+            }
+          })
+        })
+        break
+    }
+  }
+});
